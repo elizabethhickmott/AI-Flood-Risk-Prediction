80d5bfe87a5d9db081a7bf622c5968ed75baecff
Elizabeth Hickmott <psyeh8@nottingham.ac.uk>
2026-03-14 15:59:47 +0000

    complete dataset generation

--- prediction.py
+++ prediction.py
@@ -19,3 +19,22 @@ elevation = np.random.gamma(5, 120, rows)
 
 risk = [] #array to store the risk being produced
 
+for r, level, soil in zip(rainfall, river_level, soil_moisture, elevation): 
+    score = r*0.03 + level*0.8 + soil*0.01#through previous research, rainfall has some contribution. river level is super important and soil moisture is moderate, so these values are made up for now as a weighted score.
+
+    if score < 2.2:
+        risk.append(0) #low risk!
+    elif score < 3.2:
+        risk.append(1) #medium risk
+    else:
+        risk.append(2) #high risk.
+
+data = pd.DataFrame({
+    "rainfall_mm " : rainfall,
+    "river_level_m ": river_level,
+    "soil_moisture_pct ": soil_moisture,
+    "elevation_m ": elevation,
+    "flood_risk ": risk
+})
+
+data.head()
