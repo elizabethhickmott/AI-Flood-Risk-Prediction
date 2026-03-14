e9e2b74d401f57a80affe99b23bfaff374a90743
Elizabeth Hickmott <psyeh8@nottingham.ac.uk>
2026-03-14 14:45:16 +0000

    add random func

--- prediction.py
+++ prediction.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
+
+np.random.seed(42)
+
+rows = 300
